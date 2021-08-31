1cec887a2737ea3c276722bf0715cc2a226a2646
Lonely_Rifle <349461694@qq.com>
2021-08-31 17:41:28 -0400

    zz change accuracy_plot

--- accuracy result/plot_accuracy.R
+++ accuracy result/plot_accuracy.R
@@ -10,24 +10,46 @@ dat$method = as.factor(dat$method)
 
 
 #mae
-
+base_breaks <- function(n = 10){
+  function(x) {
+    axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+  }
+}
 scaleFUN <- function(x) sprintf("%.4f", x)
 p3 = dat %>% filter(m==3) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=3') + 
+  scale_y_continuous(trans = 'log10', breaks = trans_breaks('log10', function(x) 10^x),labels = trans_format('log10', math_format(10^.x))) +labs(subtitle = 'm=3') + 
   ylab("MAE")
 
+p3
+
+base_breaks <- function(n = 10){
+  function(x) {
+    axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+  }
+}
+scaleFUN <- function(x) sprintf("%.4f", x)
 p4 = dat %>% filter(m==4) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=4') + 
+  scale_y_continuous(trans = 'log10', breaks = trans_breaks('log10', function(x) 10^x),labels = trans_format('log10', math_format(10^.x))) +labs(subtitle = 'm=4') + 
   theme(axis.title.y=element_blank())
 
+p4
 
+
+
+base_breaks <- function(n = 10){
+  function(x) {
+    axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+  }
+}
+scaleFUN <- function(x) sprintf("%.4f", x)
 p5 = dat %>% filter(m==5) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=5') + 
+  scale_y_continuous(trans = log_trans(), breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=5') + 
   theme(axis.title.y=element_blank())
 
+p5
 
 p6 = dat %>% filter(m==6) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
@@ -50,40 +72,47 @@ dev.off()
 #tae
 
 
-scaleFUN <- function(x) sprintf("%.4f", x)
+scaleFUN <- function(x) sprintf("%.1f", x)
 p3 = dat %>% filter(m==3,
                     method=='simulation'|method=='normal',
                     n==10|n==20|n==30||n==40|n==50|n==60|n==70|n==80|n==90|n==100) %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=3') + 
+  scale_y_continuous(trans = log_trans(),labels = scaleFUN,breaks = seq(0,1,0.1),limits = c(0.4,0.6)) +labs(subtitle = 'm=3') + 
   ylab("TAE")
 
+p3
 p4 = dat %>% filter(m==4,
                     method=='simulation'|method=='normal',
                     n==10|n==20|n==30||n==40|n==50|n==60|n==70|n==80|n==90|n==100) %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=4') + 
   theme(axis.title.y=element_blank())
+p4
 
-
+scaleFUN <- function(x) sprintf("%.1f", x)
 p5 = dat %>% filter(m==5,method=='simulation'|method=='normal') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=5') + 
   theme(axis.title.y=element_blank())
 
+p5
 
+scaleFUN <- function(x) sprintf("%.2f", x)
 p6 = dat %>% filter(m==6,method=='simulation'|method=='normal') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=6') + 
   ylab("TAE")
 
+p6
 
+
+scaleFUN <- function(x) sprintf("%.1f", x)
 p7 = dat %>% filter(m==7,method=='simulation'|method=='normal') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method)) + 
   scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=7') + 
   theme(axis.title.y=element_blank())
 
-
+p7
 
 pdf(file = 'tae_all.pdf')
 

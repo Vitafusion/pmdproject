d846141733a58490f88b9183ce8493cdad61843a
Lonely_Rifle <349461694@qq.com>
2021-08-31 21:04:49 -0400

    zz create accu plot

--- accuracy result/plot_accuracy.R
+++ accuracy result/plot_accuracy.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(gridExtra)
 library(gtable)
 
+setwd("~/Desktop/research/pmd/pmdproject/pmdproject/accuracy result/data")
+
 dat = read.table('accura_dat.txt')
 dat$m = as.factor(dat$m)
 dat$method = as.factor(dat$method)
@@ -15,23 +17,28 @@ base_breaks <- function(n = 10){
     axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
   }
 }
+
 scaleFUN <- function(x) sprintf("%.4f", x)
+
 p3 = dat %>% filter(m==3) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = 'log10', breaks = trans_breaks('log10', function(x) 10^x),labels = trans_format('log10', math_format(10^.x))) +labs(subtitle = 'm=3') + 
+  scale_y_continuous(trans = 'log10', breaks = base_breaks()) +labs(subtitle = 'm=3') + 
   ylab("MAE")
 
 p3
+
 
 base_breaks <- function(n = 10){
   function(x) {
     axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
   }
 }
+
 scaleFUN <- function(x) sprintf("%.4f", x)
+
 p4 = dat %>% filter(m==4) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = 'log10', breaks = trans_breaks('log10', function(x) 10^x),labels = trans_format('log10', math_format(10^.x))) +labs(subtitle = 'm=4') + 
+  scale_y_continuous(trans = 'log10', breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=4') + 
   theme(axis.title.y=element_blank())
 
 p4
@@ -53,14 +60,17 @@ p5
 
 p6 = dat %>% filter(m==6) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=6') + 
+  scale_y_continuous(trans = log_trans(),breaks = base_breaks(), labels = scaleFUN) +labs(subtitle = 'm=6') + 
   ylab("MAE")
 
+p6
 
 p7 = dat %>% filter(m==7) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method)) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=7') + 
+  scale_y_continuous(trans = log_trans(),breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=7') + 
   theme(axis.title.y=element_blank())
+
+p7
 
 pdf(file = 'mae_all.pdf')
 
@@ -81,6 +91,7 @@ p3 = dat %>% filter(m==3,
   ylab("TAE")
 
 p3
+
 p4 = dat %>% filter(m==4,
                     method=='simulation'|method=='normal',
                     n==10|n==20|n==30||n==40|n==50|n==60|n==70|n==80|n==90|n==100) %>% ggplot() + 
@@ -106,10 +117,10 @@ p6 = dat %>% filter(m==6,method=='simulation'|method=='normal') %>% ggplot() +
 p6
 
 
-scaleFUN <- function(x) sprintf("%.1f", x)
+scaleFUN <- function(x) sprintf("%.3f", x)
 p7 = dat %>% filter(m==7,method=='simulation'|method=='normal') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method)) + 
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=7') + 
+  scale_y_continuous(trans = log_trans(),breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=7') + 
   theme(axis.title.y=element_blank())
 
 p7
@@ -123,7 +134,3 @@ dev.off()
 
 
 
-
-dat %>% ggplot() + 
-  geom_path(aes(x=n,y=mae,colour=method,linetype=method)) + facet_wrap(~m)
-  scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=3')

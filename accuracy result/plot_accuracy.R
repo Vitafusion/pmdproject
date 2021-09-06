29495ef20be0cf556d3902e42afbb7ce0a1df338
Lonely_Rifle <349461694@qq.com>
2021-09-06 17:06:46 -0400

    change simulation.R and make some plots

--- accuracy result/plot_accuracy.R
+++ accuracy result/plot_accuracy.R
@@ -3,14 +3,16 @@ library(scales)
 library(dplyr)
 library(gridExtra)
 library(gtable)
+library(ggpubr)
 
 setwd("~/Desktop/research/pmd/pmdproject/pmdproject/accuracy result/data")
 
-dat = read.table('accura_dat.txt')
+dat = read.table('accura_dat.txt',sep = '\t')
 dat$m = as.factor(dat$m)
 dat$method = as.factor(dat$method)
 
-
+dat.simu <- read.table('simulation.txt',sep='\t')
+dat.simu$B <- as.factor(dat.simu$B)
 #mae
 base_breaks <- function(n = 10){
   function(x) {
@@ -21,9 +23,9 @@ base_breaks <- function(n = 10){
 scaleFUN <- function(x) sprintf("%.4f", x)
 
 p3 = dat %>% filter(m==3) %>% ggplot() + 
-  geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
+  geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = T) + 
   scale_y_continuous(trans = 'log10', breaks = base_breaks()) +labs(subtitle = 'm=3') + 
-  ylab("MAE")
+  ylab("MAE") + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p3
 
@@ -39,7 +41,7 @@ scaleFUN <- function(x) sprintf("%.4f", x)
 p4 = dat %>% filter(m==4) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = 'log10', breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=4') + 
-  theme(axis.title.y=element_blank())
+  theme(axis.title.y=element_blank()) + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p4
 
@@ -54,28 +56,29 @@ scaleFUN <- function(x) sprintf("%.4f", x)
 p5 = dat %>% filter(m==5) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(), breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=5') + 
-  theme(axis.title.y=element_blank())
+  theme(axis.title.y=element_blank()) + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p5
 
 p6 = dat %>% filter(m==6) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(),breaks = base_breaks(), labels = scaleFUN) +labs(subtitle = 'm=6') + 
-  ylab("MAE")
+  ylab("MAE") + theme(plot.subtitle = element_text(hjust = 0.5))
+
 
 p6
 
 p7 = dat %>% filter(m==7) %>% ggplot() + 
   geom_path(aes(x=n,y=mae,colour=method,group=method,linetype=method)) + 
   scale_y_continuous(trans = log_trans(),breaks = base_breaks(),labels = scaleFUN) +labs(subtitle = 'm=7') + 
-  theme(axis.title.y=element_blank())
+  theme(axis.title.y=element_blank()) + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p7
 
 pdf(file = 'mae_all.pdf')
 
-grid.arrange(p3,p4,p5,p6,p7,
-             ncol = 3, nrow = 2)
+#grid.arrange(p3,p4,p5,p6,p7,ncol = 3, nrow = 2)
+ggarrange(p3,p5,p7, ncol=3, nrow=1, common.legend = TRUE, legend="bottom")
 
 dev.off()
 
@@ -84,53 +87,75 @@ dev.off()
 
 scaleFUN <- function(x) sprintf("%.1f", x)
 p3 = dat %>% filter(m==3,
-                    method=='simulation'|method=='normal',
+                    method=='SIM'|method=='N.A',
                     n==10|n==20|n==30||n==40|n==50|n==60|n==70|n==80|n==90|n==100) %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(),labels = scaleFUN,breaks = seq(0,1,0.1),limits = c(0.4,0.6)) +labs(subtitle = 'm=3') + 
-  ylab("TAE")
+  ylab("TAE") + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p3
 
 p4 = dat %>% filter(m==4,
-                    method=='simulation'|method=='normal',
+                    method=='SIM'|method=='N.A',
                     n==10|n==20|n==30||n==40|n==50|n==60|n==70|n==80|n==90|n==100) %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = log_trans(),labels = scaleFUN) +labs(subtitle = 'm=4') + 
-  theme(axis.title.y=element_blank())
+  theme(axis.title.y=element_blank()) + theme(plot.subtitle = element_text(hjust = 0.5))
 p4
 
 scaleFUN <- function(x) sprintf("%.3f", x)
-p5 = dat %>% filter(m==5,method=='simulation'|method=='normal') %>% ggplot() + 
+p5 = dat %>% filter(m==5,method=='SIM'|method=='N.A') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = 'log10',breaks = c(1,0.1,0.01,0.001),labels = scaleFUN) +labs(subtitle = 'm=5') + 
-  theme(axis.title.y=element_blank())
+  theme(axis.title.y=element_blank()) + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p5
 
 scaleFUN <- function(x) sprintf("%.3f", x)
-p6 = dat %>% filter(m==6,method=='simulation'|method=='normal') %>% ggplot() + 
+p6 = dat %>% filter(m==6,method=='SIM'|method=='N.A') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method), show.legend = F) + 
   scale_y_continuous(trans = 'log10', breaks = c(1,0.1,0.01,0.001), labels = scaleFUN) +labs(subtitle = 'm=6') + 
-  ylab("TAE")
+  ylab("TAE") + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p6
 
 
 scaleFUN <- function(x) sprintf("%.3f", x)
-p7 = dat %>% filter(m==7,method=='simulation'|method=='normal') %>% ggplot() + 
+p7 = dat %>% filter(m==7,method=='SIM'|method=='N.A') %>% ggplot() + 
   geom_path(aes(x=n,y=tae,colour=method,group=method,linetype=method)) + 
   scale_y_continuous(trans = log_trans(),breaks = c(1,0.1,0.01,0.001),labels = scaleFUN) +labs(subtitle = 'm=7') + 
-  theme(axis.title.y=element_blank())
+  theme(axis.title.y=element_blank()) + theme(plot.subtitle = element_text(hjust = 0.5))
 
 p7
 
 pdf(file = 'tae_all.pdf')
 
-grid.arrange(p3,p4,p5,p6,p7,
-             ncol = 3, nrow = 2)
+#grid.arrange(p3,p4,p5,p6,p7,ncol = 3, nrow = 2)
+
+ggarrange(p3,p5,p7, ncol=3, nrow=1, common.legend = TRUE, legend="bottom")
+
 
 dev.off()
 
 
+
+# simulation
+
+
+
+dat.simu %>% filter(B==10|B==10000|B==1e+07) %>% ggplot() + 
+  geom_path(aes(x=n,y=err.max,colour=B,group=B,linetype=B), show.legend = T) + 
+  scale_y_continuous(trans = 'log10', breaks = base_breaks()) +labs(subtitle = '100% Quantile') + 
+  ylab("error") + theme(plot.subtitle = element_text(hjust = 0.5))
+
+dat.simu %>% filter(n<40) %>% filter(B==10|B==10000|B==1e+07) %>% ggplot() + 
+  geom_path(aes(x=n,y=err.75,colour=B,group=B,linetype=B), show.legend = T) + 
+  scale_y_continuous(trans = 'log10', breaks = base_breaks()) +labs(subtitle = '75% Quantile') + 
+  ylab("error") + theme(plot.subtitle = element_text(hjust = 0.5))
+
+
+dat.simu %>% filter(n<30) %>% filter(B==10|B==10000|B==1e+07) %>% ggplot() + 
+  geom_path(aes(x=n,y=err.50,colour=B,group=B,linetype=B), show.legend = T) + 
+  scale_y_continuous(trans = 'log10', breaks = base_breaks()) +labs(subtitle = '50% Quantile') + 
+  ylab("error") + theme(plot.subtitle = element_text(hjust = 0.5))
 
---
+++ parallel/simulation2.R
@@ -0,0 +1,77 @@
+q.find <- function(res,q){
+  res.temp <- res[res!=0]
+  q.temp <- quantile(res.temp,q)
+  q.addr <- which(res==q.temp)
+  if(length(q.addr)==0){
+    dis <- abs(res.temp-q.temp)
+    temp.addr <- which(dis==min(dis))[1]
+  }
+  q.addr <- which(res==res.temp[temp.addr])
+  return(q.addr)
+}
+
+
+p.matrix <- function(n,m){
+  p <- matrix(0,nrow = n,ncol = m,byrow = T)
+  for (i in 1:n) {
+    r = runif(m)
+    r = r/sum(r) #generate row
+    r[1:(m-1)] = round(r[1:(m-1)],3)
+    while(sum(r[1:(m-1)])>1){
+        r = runif(m)
+        r = r/sum(r) #generate row
+        r[1:(m-1)] = round(r[1:(m-1)],3)
+    }
+    r[m] = 1-sum(r[1:(m-1)])
+    p[i,] = r
+  }
+  return(p)
+}
+
+
+
+library(poissonmulti)
+
+K <- 100
+m <- 3
+n <- c(1:10)
+N <- length(n)
+b <- c(10^6,10^7)
+results <- matrix(0,nrow = 1 ,ncol = 9)
+results <- as.data.frame(results)
+colnames(results) <- c("n","m","B","max","err.max","per.75","err.75","per.50","err.50")
+temp <- results
+
+
+
+print("b from 10^6 to 10^7")
+
+
+for (j in 1:length(b)) {
+  for (i in 1:N) {
+    results$B <- b[j]
+    results$n <- 10*i
+    results$m <- m
+    for(k in 1:K){
+      pp <- p.matrix(10*i,m)
+      res0 <- pmd(pp)
+      res1 <- pmd(pp, method = "simulation", t=b[j])
+      quan.res0 <- quantile(res0)
+      index.max <- which(res0 == quan.res0[5])[1]
+      index.75 <- which(res0 == quan.res0[4])[1]
+      index.50 <-  which(res0 == quan.res0[3])[1]
+      err.max <- abs(res0[index.max] - res1[index.max])
+      err.75 <- abs(res0[index.75] - res1[index.75])
+      err.50 <- abs(res0[index.50] - res1[index.50])
+      temp$max <- res0[index.max]
+      temp$err.max <- err.max
+      temp$`per.75` <- res0[index.75]
+      temp$err.75 <- err.75
+      temp$`per.50` <- res0[index.50]
+      temp$err.50 <- err.50
+      results[,4:9] <- results[,4:9] + temp[,4:9]
+    }
+    results[,4:9] <- results[,4:9]/K
+    print(results)
+  }
+}
